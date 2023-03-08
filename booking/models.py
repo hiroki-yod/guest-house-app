f21ae896bbaa07f744ae42fe10bab0aecd5b8c6c
hiroki-yod <hiroki.yod@gmail.com>
2023-03-08 21:30:15 +0900

    first commit

---
+++ booking/models.py
@@ -0,0 +1,81 @@
+import uuid
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+import uuid
+
+class CommonUser(AbstractUser):
+    uid = models.UUIDField(editable=False, default=uuid.uuid4)
+
+    REQUIRED_FIELDS = ["uid"]
+
+    def __str__(self):
+        return self.username
+
+    class Meta:
+        verbose_name = ('CommonUser')
+
+
+class HostUser(CommonUser):
+    display_name = models.CharField(max_length=100)
+    photo = models.ImageField(upload_to='images/')
+    bio = models.CharField(max_length=400, blank=True)
+    website = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.display_name
+
+    class Meta:
+        verbose_name = ('HostUser')
+
+
+class GuestUser(CommonUser):
+    display_name = models.CharField(max_length=100)
+    photo = models.ImageField(upload_to='images/')
+    bio = models.CharField(max_length=400, blank=True)
+    website = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.display_name
+
+    class Meta:
+        verbose_name = ('GuestUser')
+
+
+class Facility(models.Model):
+    uid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=100)
+    post_code = models.CharField(max_length=7)
+    address = models.CharField(max_length=200)
+    host = models.ForeignKey(to=HostUser, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return self.name
+
+
+class Room(models.Model):
+    room_id = models.PositiveSmallIntegerField()    #後でnameに変更
+    facility = models.ForeignKey(to=Facility, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.facility.name + '_' + str(self.room_id)
+
+
+class ReservationFrame(models.Model):
+    date = models.DateField()
+    is_reserved = models.BooleanField(default=False)
+    room = models.ForeignKey(to=Room, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return str(self.date) + '_' + self.room.facility.name + '_' + str(self.room.room_id)
+
+
+class Reservation(models.Model):
+    is_canceled = models.BooleanField(default=False)
+    is_checked_in = models.BooleanField(default=False)
+    check_in_date = models.DateField()
+    check_out_date = models.DateField()
+    guest = models.ForeignKey(to=GuestUser, on_delete=models.SET_NULL, null=True)   #たぶんnullじゃよくない
+    room = models.ForeignKey(to=Room, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return str(self.check_in_date) + '_' + self.room.facility.name + '_' + self.guest.display_name
