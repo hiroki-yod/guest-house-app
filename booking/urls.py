0ee73485eba08493be169e0d9c54077812a4b6da
T08Y02 <pon9q9@gmail.com>
2023-03-12 23:35:12 +0900

    施設詳細の実装

--- booking/urls.py
+++ booking/urls.py
@@ -16,6 +16,7 @@ urlpatterns = [
     # path('logout/', auth.logout_guest.as_view(), name='logout_guest'),
 
     #ここから編集 認証の仕組みがわからなかったのでとりあえずguest.pyというのをauth関係から隔離して実装
-    path('facility/index', guest.facility_guest.as_view(), name='facility_index'),
+    path('facility/index', guest.facility_index.as_view(), name='facility_index'),
+    path('facility/<uuid:facility_id>/', guest.facility_detail.as_view(), name='facility_detail'),
 
 ]
--- booking/views/guest.py
+++ booking/views/guest.py
@@ -1,10 +1,10 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.views.generic import View
 from django.shortcuts import render, redirect
 from booking.forms import auth
 from ..models import Facility
 
-class facility_guest(View):
+class facility_index(View):
     def get(self, request):
         Facility_list = Facility.objects.order_by('name')[:5]
         param = {
@@ -12,3 +12,12 @@ class facility_guest(View):
         }
         return render(request, "booking/auth/guest/facility_index.html", param)
         #return HttpResponse("You're looking at facility_index")
+
+class facility_detail(View):
+    def get(self, request, facility_id):
+        #return HttpResponse("You're looking at facility_detail")
+        try:
+            facility = Facility.objects.get(pk=facility_id)
+        except facility.DoesNotExist:
+            raise Http404("Facility does not exist")
+        return render(request, 'booking/auth/guest/facility_detail.html', {'facility': facility})
