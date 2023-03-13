9df2bc2271d73f1963d7d41e40f8e0883174cc4e
hiroki-yod <hiroki.yod@gmail.com>
2023-03-13 17:12:47 +0900

    [create] auth func

--- booking/urls.py
+++ booking/urls.py
@@ -8,8 +8,8 @@ urlpatterns = [
     #auth.py
     path('host/signup/', auth.signup_host.as_view(), name='signup_host'),
     path('signup/', auth.signup_guest.as_view(), name='signup_guest'),
-    path('login/', auth.login.as_view(), name='login'),
-    path('logout/', auth.logout.as_view(), name='logout_host'),
+    path('login/', auth.login_common.as_view(), name='login'),
+    path('logout/', auth.logout_common.as_view(), name='logout'),
     # path('signup/', auth.signup_guest.as_view(), name='signup_guest'),
     # path('login/', auth.login_guest.as_view(), name='login_guest'),
     # path('logout/', auth.logout_guest.as_view(), name='logout_guest'),
