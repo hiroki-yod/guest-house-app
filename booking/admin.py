e5ed2f1d1193725defeac8f5976e6c9464d7c741
T08Y02 <pon9q9@gmail.com>
2023-03-26 19:16:28 +0900

    一応決済できなくはない状態にはした

--- booking/admin.py
+++ booking/admin.py
@@ -1,11 +1,18 @@
 from django.contrib import admin
-from .models import CommonUser, HostUser, GuestUser, Facility, Room, ReservationFrame, Reservation
+from .models import CommonUser, HostUser, GuestUser, Facility, Room, ReservationFrame, Reservation, Price
 
+class PriceInlineAdmin(admin.TabularInline):
+    model = Price
+    extra = 0
+
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [PriceInlineAdmin]
 
 admin.site.register(CommonUser)
 admin.site.register(HostUser)
 admin.site.register(GuestUser)
 admin.site.register(Facility)
 admin.site.register(Room)
-admin.site.register(ReservationFrame)
+admin.site.register(ReservationFrame, ProductAdmin)
 admin.site.register(Reservation)
+admin.site.register(Price)
